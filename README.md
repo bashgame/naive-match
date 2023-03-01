4fa0f0153a54c5ff68673f01829f13a33682642d
bashgame <amen.kester@gmail.com>
2023-03-01 04:38:19 +0000

    Naive Tests
    Added test cases and code for naive exact matching alignment. Test for proper function using randomly generated sequence and a slice of that sequence. Test for pattern with no matches using randomly generated sequence and 'Bob'

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# Naive Match   [![Continuous Integration Workflow](https://github.com/bashgame/naive-match/actions/workflows/ci.yml/badge.svg)](https://github.com/bashgame/naive-match/actions/workflows/ci.yml)
+# Naive Match
+
+[![Continuous Integration Workflow](https://github.com/bashgame/naive-match/actions/workflows/ci.yml/badge.svg)](https://github.com/bashgame/naive-match/actions/workflows/ci.yml)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Python 3.9](https://img.shields.io/badge/Python-3.9-green.svg)](https://shields.io/)
+
 Implementation of naive exact matching for nucleotide sequences
 
 ## Testing framework
+
 Includes template code for running tests with unittest and coverage
 
 ## Containerization
+
 Includes devcontainer and Dockerfile for running in VSCode remote container
--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -1,8 +1,17 @@
 """
 Template Test Cases, please update
 """
+import random
 from unittest import TestCase
 from sequences import utils
+
+comps = {'G': 'C', 'C': 'G', 'A': 'T', 'T': 'A', 'N': 'N'}
+
+def makeSeq(length):
+    seq = ''
+    for _ in range(length):
+        seq += random.choice('ACGT')
+    return seq
 
 class TestTemplate(TestCase):
     """ Test Template, please update """
@@ -33,3 +42,19 @@ class TestTemplate(TestCase):
         res2 = utils.rev_comp(test_seq2)
         self.assertEqual(res1, 'TGTAATC')
         self.assertEqual(res2, 'TGTAATC')
+
+    def test_naive_match(self):
+        """ It should return a list of indices where a complete match occurred """
+        test_seq1 = makeSeq(100)
+        test_seq2 = test_seq1[1:4]
+        expected = test_seq1.rfind(test_seq2)
+        result = utils.naive(test_seq2, test_seq1)
+        self.assertTrue(expected in result)
+
+    def test_non_match(self):
+        """ It should return an empty list """
+        test_seq = makeSeq(100)
+        bad_seq = "BOB"
+        expected = []
+        result = utils.naive(bad_seq, test_seq)
+        self.assertEqual(expected, result)
